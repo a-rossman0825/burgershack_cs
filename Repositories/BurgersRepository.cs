e5490009f1666b3df408b535f1930e87d7ee0f95
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-07-29 15:37:10 -0600

    Burger Repository init

---
+++ Repositories/BurgersRepository.cs
@@ -0,0 +1,11 @@
+
+namespace burgershack_cs.Repositories;
+
+public class BurgersRepository
+{
+
+  public List<Burger> GetAllBurgers()
+  {
+    return [];
+  }
+}
--- Services/BurgersService.cs
+++ Services/BurgersService.cs
@@ -5,8 +5,15 @@ namespace burgershack_cs.Services;
 
 public class BurgersService
 {
+  private readonly BurgersRepository _repository;
+  public BurgersService(BurgersRepository repository)
+  {
+    _repository = repository;
+  }
+
   public List<Burger> GetAllBurgers()
   {
-    return [];
+    List<Burger> burgers = _repository.GetAllBurgers();
+    return burgers;
   }
 }
